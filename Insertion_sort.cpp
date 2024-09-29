f456d90d7dcaf035e314d8afe64f30657e555e10
Asfia-oishy <asfiamoon126@gmail.com>
2024-09-29 18:16:38 +0600

    insertion_sort

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,12 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void swap(int &a, int &b)
-{
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
 
 int main()
 {
+
+    int arr[] = {1, 4, 7, 2, 3, 6, 9, 1};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    //algo
+
+    for(int i=1; i<n;i++)
+    {
+        int val = arr[i];
+        int hole =i;
+        while(hole>0 && arr[hole-1]>val)
+        {
+            arr[hole] = arr[hole-1];
+            hole--;
+            
+        }
+        arr[hole] = val;
+    }
+
+
+    
+    cout << "sorted - " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
