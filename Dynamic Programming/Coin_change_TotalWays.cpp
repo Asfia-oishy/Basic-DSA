73ecea8fbf3c84c0ede03e91479edccb7891cb12
Asfia-oishy <asfiamoon126@gmail.com>
2024-10-03 13:10:13 +0600

    updated

--- Dynamic Programming/Coin_change_TotalWays.cpp
+++ Dynamic Programming/Coin_change_TotalWays.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//here 1st row is filled with 0
+
 int main()
 {
-    vector<int> coins = {0, 2, 3, 5, 10}; // Coin denominations
-    int change = 15;                      // Total amount of change needed
+    vector<int> coins = {2, 3, 5, 10}; // Coin denominations
+    int change = 15;                   // Total amount of change needed
 
     // 2D vector to store results, initialized to 0
     vector<vector<int>> a(coins.size() + 1, vector<int>(change + 1, -1));
 
+    for (int i = 0; i <= change; i++)
+        a[0][i] = 0; //1st row
+
+    a[0][0] = 1;
+
     // Iterate over the coins
-    for (int i = 0; i <= coins.size(); i++)
+    for (int i = 1; i <= coins.size(); i++)
     {
         for (int j = 0; j <= change; j++)
         {
@@ -19,25 +26,21 @@ int main()
                 // There's one way to make 0 change: by not using any coins
                 a[i][j] = 1;
             }
-            else if (i == 0)
-            {
-                a[i][j] = 0;
-            }
-            else if (coins[i] > j)
+
+            else if (coins[i - 1] > j)
             {
                 // If the coin value is greater than the current amount, exclude this coin
-                if (i > 0)
                     a[i][j] = a[i - 1][j];
             }
             else
             {
-                a[i][j] = a[i - 1][j] + a[i][j - coins[i]]; // Include or exclude the coin
+                a[i][j] = a[i - 1][j] + a[i][j - coins[i - 1]]; // Include or exclude the coin
             }
         }
     }
 
     // Output the number of ways to make the change
-    cout << a[coins.size() - 1][change] << endl;
+    cout << a[coins.size()][change] << endl;
 
     return 0;
 }
---
+++ Dynamic Programming/minimumCoinChange.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//here 1st row is filled with 0
+
+int main()
+{
+
+
+    return 0;
+}
