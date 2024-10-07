0914018dfc2ca5bd7a684dcb4c325f39640df045
Asfia-oishy <asfiamoon126@gmail.com>
2024-10-07 20:01:18 +0600

    another recursion

--- CombinationSumRecursion.cpp
+++ CombinationSumRecursion.cpp
@@ -1,8 +1,14 @@
-void func(int ind, int sum, vector<int> &v, int len, vector<int> &ans, int k)
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//type 1
+/*
+void findCombSum(int ind, vector<int> &v, vector<int> &ans, int target, int len)
 {
     if (ind == len)
     {
-        if (sum == k)
+        if (target == 0)
         {
             for (int i = 0; i < ans.size(); i++)
                 cout << ans[i] << " ";
@@ -10,22 +16,71 @@ void func(int ind, int sum, vector<int> &v, int len, vector<int> &ans, int k)
         }
         return;
     }
-    ans.push_back(v[ind]);
-    sum += v[ind];
-    func(ind + 1, sum, v, len, ans, k);
-    ans.pop_back();
-    sum -= v[ind];
-    func(ind + 1, sum, v, len, ans, k);
-    return;
+    if (v[ind] <= target)
+    {
+        ans.push_back(v[ind]);
+        findCombSum(ind, v, ans, target - v[ind], len);
+        ans.pop_back();
+    }
+    findCombSum(ind + 1, v, ans, target, len);
 }
 
 int main()
 {
-    vector<int> v = {1, 2, 1};
+
+    vector<int> v = {2, 3, 6, 7};
+
     vector<int> ans;
+    int target = 7;
     int n = v.size();
-    int k = 2;
-    func(0, 0, v, n, ans, k);
+
+    findCombSum(0, v, ans, target, n);
+
+    return 0;
+}
+*/
+
+//type 2
+void findCombSum(int ind, vector<int> &v, vector<int> &ans, int target, int len,map<vector<int>,int> &mp)
+{
+    if (ind == len)
+    {
+        if (target == 0)
+        {
+            sort(ans.begin(),ans.end());
+            if(mp.find(ans)==mp.end())
+            {
+                mp[ans]++;
+                for(int i=0;i<ans.size();i++)
+                {
+                    cout<<ans[i]<<" ";
+                }
+                cout<<endl;
+                
+            }
+        }
+        return;
+    }
+    if (v[ind] <= target)
+    {
+        ans.push_back(v[ind]);
+        findCombSum(ind+1, v, ans, target - v[ind], len,mp);
+        ans.pop_back();
+    }
+    findCombSum(ind + 1, v, ans, target, len,mp);
+}
+
+int main()
+{
+
+    vector<int> v ={1,1,1,2,2};
+
+    vector<int> ans;
+    int target = 4;
+    int n = v.size();
+    map<vector<int>,int> mp;
+
+    findCombSum(0, v, ans, target, n,mp);
 
     return 0;
 }
